aab7dbab82c15d3251f9940f4cb8f2464c32bf02
KolyadaS <kolyadas89@gmail.com>
2022-04-11 15:10:39 +0400

    Add new rooms to hotelRooms.js

--- src/hotelRooms.js
+++ src/hotelRooms.js
@@ -25,25 +25,124 @@ hotelRooms.push(room);
 
 room = {
     id: "0003",
-    suite: 111,
-    luxury: false,
-    price: 1111,
-    rating: 1,
-    feedback: 11,
-    picture: ['../blocks/images/0003_3.png', '../blocks/images/0003_1.jpeg', '../blocks/images/0003_2.png']
+    suite: 980,
+    luxury: true,
+    price: 8500,
+    rating: 3,
+    feedback: 35,
+    picture: ['../blocks/images/0003_1.png']
 }
 hotelRooms.push(room);
 
 room = {
     id: "0004",
-    suite: 555,
+    suite: 856,
     luxury: false,
-    price: 555,
+    price: 7300,
     rating: 5,
-    feedback: 55,
-    picture: []
+    feedback: 19,
+    picture: ['../blocks/images/0004_1.png']
 }
 hotelRooms.push(room);
+
+room = {
+    id: "0005",
+    suite: 740,
+    luxury: false,
+    price: 6000,
+    rating: 4,
+    feedback: 44,
+    picture: ['../blocks/images/0005_1.png']
+}
+hotelRooms.push(room);
+
+room = {
+    id: "0006",
+    suite: 982,
+    luxury: false,
+    price: 5800,
+    rating: 3,
+    feedback: 56,
+    picture: ['../blocks/images/0006_1.png']
+}
+hotelRooms.push(room);
+
+room = {
+    id: "0007",
+    suite: 678,
+    luxury: false,
+    price: 5500,
+    rating: 5,
+    feedback: 45,
+    picture: ['../blocks/images/0007_1.png']
+}
+hotelRooms.push(room);
+
+room = {
+    id: "0008",
+    suite: 450,
+    luxury: false,
+    price: 5300,
+    rating: 4,
+    feedback: 39,
+    picture: ['../blocks/images/0008_1.png']
+}
+hotelRooms.push(room);
+
+room = {
+    id: "0009",
+    suite: 350,
+    luxury: false,
+    price: 5000,
+    rating: 3,
+    feedback: 77,
+    picture: ['../blocks/images/0009_1.png']
+}
+hotelRooms.push(room);
+
+room = {
+    id: "0010",
+    suite: 666,
+    luxury: false,
+    price: 5000,
+    rating: 5,
+    feedback: 25,
+    picture: ['../blocks/images/0010_1.png']
+}
+hotelRooms.push(room);
+
+room = {
+    id: "0011",
+    suite: 444,
+    luxury: false,
+    price: 5000,
+    rating: 3,
+    feedback: 15,
+    picture: ['../blocks/images/0011_1.png']
+}
+hotelRooms.push(room);
+
+room = {
+    id: "0012",
+    suite: 352,
+    luxury: false,
+    price: 5000,
+    rating: 3,
+    feedback: 55,
+    picture: ['../blocks/images/0012_1.png']
+}
+hotelRooms.push(room);
+
+
+
+
+
+
+
+
+
+
+
 
 // console.log("хотелрумный js = " + hotelRooms);
 // console.log(JSON.stringify(hotelRooms, null, '  '));
