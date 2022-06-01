cddee8c575966739907fc06fe48c34324a088ffa
KolyadaS <kolyadas89@gmail.com>
2022-06-01 15:32:43 +0400

    refactor room-card.js

--- src/blocks/room-card/room-card.js
+++ src/blocks/room-card/room-card.js
@@ -1,6 +1,184 @@
 import {hotelRooms} from '../../hotelRooms.js';
+// export default createAllThis;
+export default createRoomMiniCard;
 
-// получаем коллекцию всех карточек с комнатами
+
+// createAllThis();
+
+
+function createRoomMiniCard(roomObj, nodeToAppend) {
+        
+    function createDiv(classes, placeToAppend) {
+        let newDiv = document.createElement('div');
+        newDiv.className += classes;
+        placeToAppend.append(newDiv);
+        return newDiv;
+    }
+
+    let divElem = createDiv("room-card", nodeToAppend);
+    divElem.setAttribute('data-room-id', roomObj['id']);
+    let currentRoomObj = divElem;
+    
+    nodeToAppend = divElem;
+    divElem = createDiv("slider-line slider-line-" + roomObj['id'], nodeToAppend);
+    createSliderLine(roomObj, divElem);
+
+    divElem = createDiv("slider-dots slider-dots-" + roomObj['id'], nodeToAppend);
+    divElem = createDiv("room-card__image_prev room-card__image_prev-" + roomObj['id'], nodeToAppend);
+    divElem = createDiv("room-card__image_next room-card__image_next-" + roomObj['id'], nodeToAppend);
+    divElem = createDiv("room-card__details", nodeToAppend);
+
+    
+    
+    
+    
+    
+    drawSlider(currentRoomObj);
+
+    function drawSlider(roomObject) {
+        let offset = 0;
+        const sliderLine = roomObject.querySelector('.slider-line');
+        let imgCount = sliderLine.children.length;
+    
+        let dotFilled;
+        createImageDots(imgCount);
+    
+        function createImageDots(imageCount) {
+            console.log('Нужно точек: ' + imageCount);
+            let left0 = 247;
+            let left1;
+            let top0 = 0;
+            let top1;
+            let dotEmpty;
+    
+            for (var i = 1; i <= imageCount; i++) {
+                left1 = left0;
+                top1 = top0;
+                dotEmpty = document.createElement('div');
+                dotEmpty.className = "dot dot"+i;
+                dotEmpty.style.left = left1 + "px";
+                dotEmpty.style.top = top1 + "px";
+                roomObject.querySelector('.slider-dots').append(dotEmpty);
+                left0 -= 14;
+                top0 -= 9;
+            }
+    
+            dotFilled = document.createElement('div');
+            dotFilled.className = "dot dot0";
+            dotFilled.style.left = left1 + "px";
+            top1 -= 9;
+            dotFilled.style.top = top1 + "px";
+            dotFilled.style.backgroundImage = "url(../blocks/images/ellipse_filled.svg)";
+            dotFilled.style.zIndex = "40";
+            roomObject.querySelector('.slider-dots').append(dotFilled);
+        }
+    
+        roomObject.querySelector('.room-card__image_next').addEventListener('click', function(){
+            offset += 270;
+            if (offset > 270*(imgCount-1)) {
+                offset = 0;
+            }
+            sliderLine.style.left = -1 * offset + 'px';
+    
+                    // изменение положения закрашенной точки
+            let dotFilledX = dotFilled.style.left.slice(0, dotFilled.style.left.length-2);
+            if (dotFilledX < 247) {
+                dotFilled.style.left = String(Number(dotFilledX) + 14) + "px";
+            }
+            else {
+                dotFilled.style.left = String(247 - 14*(imgCount-1)) + "px";
+            }   
+        })
+    
+        roomObject.querySelector('.room-card__image_prev').addEventListener('click', function(){
+            offset -= 270;
+            if (offset < 0) {
+                offset = 270*(imgCount-1);
+            }
+            sliderLine.style.left = -1 *offset + 'px';
+    
+                    // изменение положения закрашенной точки
+            let dotFilledX = dotFilled.style.left.slice(0, dotFilled.style.left.length-2);
+            if (dotFilledX > 270-15-8-14*(imgCount-1)) {                        // > 205
+                dotFilled.style.left = String(Number(dotFilledX) - 14) + "px";
+            }
+            else {
+                dotFilled.style.left = "247px";
+            }
+        })
+    
+    }
+
+
+
+
+
+
+
+
+    nodeToAppend = divElem;
+    divElem = createDiv("details__suite font_h3", nodeToAppend);
+    divElem.innerHTML = "№ <span class='font_h1'>000</span>";
+    divElem = createDiv("details__price font_body", nodeToAppend);
+    divElem.innerHTML = '<span class="price-and-ruble-sign">0</span> в сутки';
+    divElem = createDiv("room-card__details_horizontal-line", nodeToAppend);
+    divElem = createDiv("details__rating", nodeToAppend);
+
+    // +rate-button("rating-"+roomID)
+
+    divElem = createDiv("details__feedback font_body", nodeToAppend);
+    divElem.innerHTML = 'отзывов нет';
+}
+
+
+
+
+function createSliderLine(roomObj, placeToAppend) {
+    let imageArr = roomObj["picture"];
+    // console.log("images: ");
+    // console.log(imageArr);
+    if (imageArr.length > 0) {
+        imageArr.forEach(element => {                          // для каждого элемента в массиве picture создать img
+            let imgElem = document.createElement('img');
+            imgElem.src = String(element);
+            imgElem.alt = "apartment " + String(roomObj['id']);
+            imgElem.style.width = "270px";
+            placeToAppend.append(imgElem); 
+        });
+    }
+    else {
+        let noImgElem = document.createElement('img');
+        noImgElem.src = "../blocks/images/no-image-icon.png";
+        noImgElem.alt = "Изображение отсутствует";
+        noImgElem.style.marginLeft = "35px";
+        noImgElem.style.marginTop = "auto";
+        placeToAppend.append(noImgElem);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function createAllThis() {
+    // получаем коллекцию всех карточек с комнатами
 let currentRoom = document.getElementsByClassName('room-card');
 console.log("Все комнаты: ");
 console.log(currentRoom);
@@ -139,5 +317,7 @@ function drawSlider(roomObject) {
             dotFilled.style.left = "247px";
         }
     })
+
+}
 
 }
