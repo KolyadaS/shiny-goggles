9119768018bd60dea57b17fdc6650726672f16a8
KolyadaS <kolyadas89@gmail.com>
2022-07-04 15:37:49 +0400

    Toggle visibility for page-main and page-search-room blocks

--- src/index.js
+++ src/index.js
@@ -10,3 +10,13 @@ import "./blocks/rate-button/rate-button.js";
 import "./blocks/room-card/room-card.js";
 import "./blocks/dropdown/dropdown.js";
 import './blocks/page-search-room/page-search-room.js';
+
+
+const searchRoomButton = document.getElementsByClassName('search-form__button');
+const homeContainer = document.getElementsByClassName('home-container');
+const searchRoomContainer = document.getElementsByClassName('search-room-container_display');
+
+searchRoomButton[0].onclick = function (e) {
+    searchRoomContainer[0].style.display = "flex";
+    homeContainer[0].style.display = "none";
+}  
