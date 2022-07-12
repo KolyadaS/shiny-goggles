e24b5fae70554c79dc94e64ee5bfa36cdea9622b
KolyadaS <kolyadas89@gmail.com>
2022-07-12 17:24:24 +0400

    Refactor page-search-room

--- src/blocks/page-search-room/page-search-room.js
+++ src/blocks/page-search-room/page-search-room.js
@@ -1,11 +1,8 @@
-import {hotelRooms} from '../../hotelRooms.js';
+import { hotelRooms } from '../../hotelRooms.js';
 import createRoomMiniCard from '../room-card/room-card.js';
 
-console.log('page-search-room.js: room searching...');
 let roomsCollection = hotelRooms;
 let roomsWrapper = document.getElementsByClassName('rooms-wrapper');
-console.log(roomsWrapper);
-console.log(roomsCollection);
 roomsCollection.forEach(element => {
     let divElem = document.createElement('div');
     divElem.className += "room";
